703b7efe3c1a05afe8ddfa6245f09e99be493e32
ashnapaularackal <ashnapaularackal@gmail.com>
2024-07-16 18:02:04 -0400

    Header is updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../slices/authSlice'; // Assuming logout action is defined in authSlice
+import { logout } from '../slices/authSlice';
+import './Header.css'; // Import the CSS file
 
 const Header = () => {
     const dispatch = useDispatch();
@@ -9,51 +10,48 @@ const Header = () => {
     const { userInfo } = useSelector((state) => state.auth);
 
     const handleLogout = () => {
-        dispatch(logout()); // Dispatch logout action
-        navigate('/'); // Redirect to login page after logout
+        dispatch(logout());
+        navigate('/');
     };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand navbar-light bg-light">
             <div className="container-fluid">
-            <Link to="/dashboard" className="navbar-brand">
-    <img
-        className="element-telus-logo"
-        alt="Element telus logo"
-        src="/images/telus.png"
-        height="40"
-    />
-</Link>
-                <div className="collapse navbar-collapse justify-content-end">
-                    <ul className="navbar-nav">
-                        {!userInfo ? (
-                            <>
-                                <li className="nav-item">
-                                    <Link to="/login" className="nav-link">
-                                        Login
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/register" className="nav-link">
-                                        Sign Up
-                                    </Link>
-                                </li>
-                            </>
-                        ) : (
-                            <> 
-               
-                                <li className="nav-item">
-                                    <button
-                                        className="btn btn-outline-danger"
-                                        onClick={handleLogout}
-                                    >
-                                        Logout
-                                    </button>
-                                </li>
-                            </>
-                        )}
-                    </ul>
-                </div>
+                <Link to="/dashboard" className="navbar-brand">
+                    <img
+                        className="element-telus-logo"
+                        alt="Element telus logo"
+                        src="/images/telus.png"
+                        height="40"
+                    />
+                </Link>
+                <ul className="navbar-nav ms-auto">
+                    {!userInfo ? (
+                        <>
+                            <li className="nav-item">
+                                <Link to="/login" className="nav-link nav-link-custom">
+                                    Login
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/register" className="nav-link nav-link-custom">
+                                    Sign Up
+                                </Link>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li className="nav-item">
+                                <button
+                                    className="btn btn-outline-danger"
+                                    onClick={handleLogout}
+                                >
+                                    Logout
+                                </button>
+                            </li>
+                        </>
+                    )}
+                </ul>
             </div>
         </nav>
     );
