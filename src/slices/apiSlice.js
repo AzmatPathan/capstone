aedd1f02a671eec256ca02355abaefdea7c4c516
Azmat Pathan <azmat.08pathan@gmail.com>
2024-08-02 10:22:02 -0400

    backend

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,11 +1,12 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const BASE_URL = 'http://localhost:3000';
+// Use environment variable for base URL
+const BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
 export const apiSlice = createApi({
     baseQuery,
-    tagTypes: ['Dashboard','User','Review'],
+    tagTypes: ['Dashboard', 'User', 'Review'],
     endpoints: (builder) => ({}),
 });
